8620ef32ab45208c0aeb44a22052e8141c8ff5d6
Jerson La Torre <jerson.latorre@gmail.com>
2024-04-07 03:05:29 -0500

    update

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,10 +5,10 @@ set -e
 
 # Si hay cambios, proceder con el proceso de compilación, add, commit, y push
 echo "Compilando..."
-node_modules/.bin/parcel build ./src/hand-detector.js --dist-dir ./dist
+node_modules/.bin/parcel build ./src/hand-tracker.js --dist-dir ./dist
 
 # Envía el archivo compilado a /Users/uno/Desktop/blazepose-html
 echo "Enviando archivo compilado a /Users/uno/Desktop/handpose-html..."
-cp ./dist/hand-detector.js /Users/uno/Desktop/handpose-html/hand-detector.js
+cp ./dist/hand-tracker.js /Users/uno/Desktop/handpose-html/hand-tracker.js
 
 echo "Listo!"
